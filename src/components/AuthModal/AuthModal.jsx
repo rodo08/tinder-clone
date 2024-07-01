9b3eb8be7dcdde346ab5f0fef882f6d7309fe949
rodo08 <rodrigo.rosalesmoya@gmail.com>
2024-07-01 14:23:44 +0200

    Add react-cookie

--- src/components/AuthModal/AuthModal.jsx
+++ src/components/AuthModal/AuthModal.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { useCookie, useCookies } from "react-cookie";
 
 const AuthModal = ({ setShowModal, isSignUp }) => {
   const [email, setEmail] = useState(null);
   const [password, setPassword] = useState(null);
   const [confirmPassword, setConfirmPassword] = useState(null);
   const [error, setError] = useState(null);
+  const [cookies, setCookie, removeCookie] = useCookies(null);
+
   const navigate = useNavigate();
 
   const handleClick = () => {
