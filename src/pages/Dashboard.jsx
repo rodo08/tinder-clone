8110effa7cbc5502e9477e7787a631c74eeb2c5c
rodo08 <rodrigo.rosalesmoya@gmail.com>
2024-07-02 11:35:39 +0200

    Add get chosen users type by logged user from db

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,32 @@
 import TinderCard from "react-tinder-card";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useCookies } from "react-cookie";
 import ChatContainer from "../components/ChatContainer/ChatContainer";
+import axios from "axios";
 
 const Dashboard = () => {
+  const [user, setUser] = useState(null);
+  const [cookies, setCookie, removeCookie] = useCookies(["user"]);
+
+  const userId = cookies.UserId;
+
+  const getUser = async () => {
+    try {
+      const response = await axios.get("http://localhost:8000/user", {
+        params: { userId },
+      });
+      setUser(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getUser();
+  });
+
+  console.log("user", user);
+
   const characters = [
     {
       name: "Richard Hendricks",
